5dd73b4391b3bbf5d4b8a14a5bc1e235c83723fa
Armani Araujo <aaraujo3@my.yorku.ca>
2024-03-07 20:58:49 -0500

    First commit

---
+++ src/test/java/com/armani/TBAService/TbaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armani.TBAService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
